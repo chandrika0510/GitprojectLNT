12ac4e3580dfaca1f29c5125eb5d5f2975ed7ecf
chandrika0510 <100669878+chandrika0510@users.noreply.github.com>
2022-03-03 17:09:10 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -3,8 +3,4 @@ public static void main(String []  args){
 system.out.println("Hello its Chandu")
 int a=10;
 System.out.println(a);
-String s= "Hello";
-int b=10;
->>>>>>>>>5f35f07a7bfc095965d2efcb1b8948f57028d0c8
-int c=10;
 }}
